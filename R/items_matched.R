34a3b1d42a425c47d9f87870a6cad55a6ea4de89
Rolf Simoes <rolfsimoes@gmail.com>
2020-07-11 13:32:32 -0300

    Implements `items_matched` function

---
+++ R/items_matched.R
@@ -0,0 +1,35 @@
+#' @title Items functions
+#'
+#' @author Rolf Simoes
+#'
+#' @description The \code{items_matched} returns how many items matched the
+#' search criteria. It implements \code{search:metadata} STAC API
+#' extension (v0.8.0).
+#'
+#' @param items    A \code{stac_items} object.
+#'
+#' @return
+#' An \code{integer} value. If STAC web server does not support this
+#' extension, returns \code{NULL}.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' stac_search("http://brazildatacube.dpi.inpe.br/bdc-stac/0.8.0") %>%
+#'     stac_request() %>%
+#'     items_matched()
+#' }
+#'
+#' @export
+items_matched <- function(items) {
+
+  # TODO: check stac API version and find properly field
+  # STAC API (<=0.8.0): "search:metadata"
+  # STAC API (>=0.9.0): "context"
+  # How to check STAC API version:
+  # Maybe `stac_version` field.
+  if (!inherits(items, "stac_items"))
+    stop(sprintf("Invalid `stac_items` object."))
+
+  return(items[["search:metadata"]][["matched"]])
+}
