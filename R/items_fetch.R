612e8dc112dc75f89fa7f2778ad1145685bda0be
Rolf Simoes <rolfsimoes@gmail.com>
2020-07-13 01:33:55 -0300

    `items_fetch` documentation

---
+++ R/items_fetch.R
@@ -0,0 +1,44 @@
+#' @title STAC functions
+#'
+#' @author Rolf Simoes
+#'
+#' @description The \code{stac_request} is function that makes HTTP
+#' requests to STAC web services, retrieves, and parse the data.
+#'
+#' @param items      A \code{stac_items} object representing the result of
+#' \code{/stac/search}, \code{/collections/{collectionId}/items}, or
+#' \code{/collections/{collectionId}/items/{itemId}} endpoints.
+#'
+#' @param headers    A \code{list} of named arguments to be passed as
+#' http request headers. This is used in \emph{addition} to eventual headers
+#' defined in \code{stac} object parameter.
+#'
+#' @seealso
+#' \code{\link{stac}} \code{\link{stac_search}} \code{\link{stac_collections}}
+#' \code{\link{stac_items}}
+#'
+#' @return
+#' A \code{stac_items} object.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' stac_search(url = "http://brazildatacube.dpi.inpe.br/bdc-stac/0.8.0",
+#'             collections = "MOD13Q1",
+#'             bbox = c(-55.16335, -4.26325, -49.31739, -1.18355)) %>%
+#'     stac_request() %>%
+#'     items_fetch()
+#' }
+#'
+#' @export
+items_fetch <- function(items, headers = list()) {
+
+  if (!inherits(items, "stac_items"))
+    stop(sprintf("Invalid `stac_items` object."), call. = FALSE)
+
+  s <- attr(items, "stac")
+  content <- stac_request(s, method = s$method)
+  return(content)
+}
+
+
