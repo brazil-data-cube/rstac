628d876049ef9735212e022ce1ec032e99140fbb
Felipe <lipecaso@gmail.com>
2020-07-17 01:19:42 -0300

    Adding interscts parameter + function .stac_version.r + tests + function items_assets

--- R/assets_download.R
+++ R/assets_download.R
@@ -6,9 +6,13 @@
 #'
 #' @param res ...
 #' @param output_dir ...
+#' @param assets_name ...
 #'
 #' @export
-assets_download <- function(res, output_dir = "./", curl_hearder = list()){
+#'
+#'
+assets_download <- function(res, output_dir = "./", curl_hearder = list(),
+                            assets_name = c()){
 
   # check the object class
   if (!inherits(res, "stac_items"))
@@ -24,6 +28,8 @@ assets_download <- function(res, output_dir = "./", curl_hearder = list()){
   # items_total <- rstac::items_fetch()
   # TODO: Verificar a quantidade de items <<- item_fetch
   # TODO: Ajustar a barra de download
+  # TODO: verificar como baixar as imagens cortadas usando vci direto do servidor
+
 
   # pb <- progress::progress_bar$new(
   #   format = "  downloading [:bar] :percent eta: :eta",
@@ -38,6 +44,8 @@ assets_download <- function(res, output_dir = "./", curl_hearder = list()){
     for (asset in 1:length(assets)) {
       asset_name <- names(assets[asset])
       asset_href <- assets[[asset]]$href
+      # TODO: criar um regex para extrair a ext do arquivo: se é
+      # .tif ou .png
 
       #TODO:  Verificar o formato do arquivo antes de baixor
       curl::curl_download(url      = asset_href,
@@ -47,10 +55,36 @@ assets_download <- function(res, output_dir = "./", curl_hearder = list()){
                                              asset_name),
                           handle   =  curl_hearder)
     }
-
   }
 
+  # Retorno stac_items com a  url apontada para cada item da maquina o usuario
   return(invisible(NULL))
 }
 
+#'
+#'
+#'
+#'
+items_assets <- function(obj_stac){
 
+  if (!inherits(obj_stac, "stac_items"))
+    stop(sprintf("Invalid `stac_items` object."), call. = FALSE)
+
+  items_len <- rstac::items_length(obj_stac)
+  if (items_len == 0)
+    stop(sprintf("Query provided returned 0 items.
+                 Please verify your query"), call. = FALSE)
+
+  res_features <- obj_stac$features
+  items_assets <- list()
+
+  for (feature in 1:items_len) {
+    assets_name <- names(res_features[[feature]][["assets"]])
+    feature_id  <- res_features[[feature]][["id"]]
+
+    items_assets[[feature]] <- list(collection_name = feature_id,
+                                    assets_name = assets_name)
+  }
+
+  return(items_assets)
+}
