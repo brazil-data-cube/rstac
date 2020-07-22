783af6a3081e37f25e0732212ed13c3810707d7b
Rolf Simoes <rolfsimoes@gmail.com>
2020-07-22 15:26:12 -0300

    updates `items_fetch` function

--- R/items_fetch.R
+++ R/items_fetch.R
@@ -9,6 +9,9 @@
 #'  \code{/stac/search}, \code{/collections/{collectionId}/items}, or
 #'  \code{/collections/{collectionId}/items/{itemId}} endpoints.
 #'
+#' @param progress   A \code{logical} indicating if a progress bar must be
+#' shown or not. Defaults to \code{TRUE}.
+#'
 #' @param headers    A \code{list} of named arguments to be passed as
 #'  http request headers. This is used in \emph{addition} to eventual headers
 #'  defined in \code{stac} object parameter.
@@ -31,20 +34,76 @@
 #' }
 #'
 #' @export
-items_fetch <- function(items, headers = list()) {
+items_fetch <- function(items, progress = TRUE, headers = list()) {
 
-  if (!inherits(items, "stac_items", "stac_item"))
+  if (!inherits(items, c("stac_items", "stac_item")))
     stop(sprintf("Invalid `stac_items` object."), call. = FALSE)
+
+  matched <- items_matched(items)
+
+  # verify if progress bar can be shown
+  progress <- progress & (!is.null(matched) && (items_length(items) < matched))
+  if (progress)
+    pb <- utils::txtProgressBar(min = items_length(items), max = matched,
+                                style = 3)
+
+  while (TRUE) {
+
+    # protect against infinite loop
+    if (!is.null(matched) && (items_length(items) > matched))
+      stop(sprintf(paste("Length of returned items (%s) is different",
+                         "from matched items (%s)."),
+                   items_length(items), matched), call. = FALSE)
+
+    # get url of the next page
+    next_stac <- .items_get_next_stac(items)
+    if (is.null(next_stac))
+      return(items)
+
+    # get request method
+    request <- attr(items, "request")
+    if (is.null(request))
+      request <- list(method = "get")
+
+    # call request
+    content <- stac_request(next_stac, method = request$method,
+                            post_enctype = request$post_enctype)
+
+    if (!inherits(content, "stac_items"))
+      stop(sprintf("Invalid content response."), call. = FALSE)
+
+    # merge features result into resulting content
+    content$features <- c(items$features,
+                          content$features)
+
+    # update progress bar
+    if (progress)
+      utils::setTxtProgressBar(pb, items_length(items))
+
+    # prepares next iteration
+    items <- content
+  }
+
+  # close progress bar
+  if (progress)
+    close(pb)
+
+  return(items)
+}
+
+
+.items_get_next_stac <- function(items) {
+
+  s <- attr(items, "stac")
+  if (is.null(s))
+    return(NULL)
 
   next_url <- Filter(function(x) x$rel == "next", items$links)
   if (length(next_url) == 0)
-    return(items)
+    return(NULL)
 
-  next_url <- next_url[[1]]$href
-
-  s <- attr(items, "stac")
-  # if (is.null(s) || (inherits(s, "stac") && s$))
-
-  content <- stac_request(s, method = s$method)
-  return(content)
+  # update stac object with params of the next url
+  next_stac <- .url_to_stac(next_url[[1]]$href)
+  next_stac$expected_responses <- s$expected_responses
+  return(next_stac)
 }
