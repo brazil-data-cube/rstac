37ea3bd0acadf53cdb179c7974e488cf29cb0515
Rolf Simoes <rolfsimoes@gmail.com>
2020-07-25 20:03:36 -0300

    `.check_response` transfered to stac_utils.R

--- R/rstac_utils.R
+++ R/rstac_utils.R
@@ -146,6 +146,47 @@
 
 #' @title STAC utils
 #'
+#' @author Rolf Simoes
+#'
+#' @description The \code{.check_response} function that checks if the request's
+#' response is in accordance with the \code{expected} parameters.
+#'
+#' @param res  a \code{httr} \code{response} object.
+#'
+#' @param expected a \code{list} containing the expected parameters values.
+#'
+#' @return a \code{character} with document class
+.check_response <- function(res, expected) {
+
+  method <- expected[[tolower(res$request$method)]]
+  if (is.null(method))
+    .error("HTTP method '%s' not defined for this operation.", res$method)
+
+  # TODO: validate stac response
+  # .stac_response_type(res, expected)
+
+  status_code <- method$responses[[as.character(res$status_code)]]
+  if (is.null(status_code)) {
+    content <- httr::content(res)
+    if (!is.null(content$code))
+      .error("%s %s", content$code, content$description)
+    .error("HTTP status '%s' not defined for this operation.",
+           res$status_code)
+  }
+  content_type  <- httr::http_type(res)
+  content_class <- status_code[[content_type]]
+  if (is.null(content_class))
+    .error("HTTP content type response '%s' not defined for this operation.",
+           content_type)
+
+  if (content_class == "")
+    content_class <- NULL
+
+  return(content_class)
+}
+
+#' @title STAC utils
+#'
 #' @rdname http_request
 #'
 #' @description
@@ -206,30 +247,6 @@
                  sapply(unname(params), paste0, collapse = ","),
                  sep = "=", collapse = "&"))
   return(paste0(params, collapse = ","))
-}
-
-#' @title STAC utils
-#'
-#' @author Rolf Simoes
-#'
-#' @description The \code{.url_to_stac} ...
-#'
-#' @param url ...
-#'
-#' @return ...
-.url_to_stac <- function(url) {
-
-  url <- url[[1]]
-
-  base_url <- gsub("^([^?]+)\\?(.*)$", "\\1", url)
-  query <- ""
-  if (grepl("^([^?]+)\\?(.*)$", url))
-    query <- gsub("^([^?]+)\\?(.*)$", "\\2", url)
-
-  s <- structure(list(url = base_url,
-                      params = .query_decode(query)),
-                 class = "stac")
-  return(s)
 }
 
 #' @title STAC utils
