9abe82e24d91c04f67360b46251a2918b1dc6f9c
Felipe <lipecaso@gmail.com>
2020-10-13 00:50:16 -0300

    Add new items functions; updating documentation

--- R/utils.R
+++ R/utils.R
@@ -412,30 +412,136 @@ stac_version <- function(x, ...) {
 
 #' @title Utility functions
 #'
-#' @description
-#' This function retrieves the names of subfields in \code{RSTACDocument}
-#'  object.
+#' @description This function returns the \code{date}, \code{band} and
+#'  \code{URL} fields for each assets of an \code{STACItemCollection} object.
+#'  For the URL you can add the GDAL library drivers for the following schemes:
+#'  HTTP/HTTPS files, S3 (AWS S3) and GS (Google Cloud Storage).
 #'
-#' @param x a \code{list} that represents the subfields of a field of
-#'  the \code{RSTACDocument} object.
+#' @param items               a \code{STACItemCollection} object representing
+#'  the result of \code{/stac/search}, \code{/collections/{collectionId}/items}.
 #'
-#' @return a \code{character} with the names of the subfields.
+#' @param assets_names        a \code{character} with the assets names to be
+#'  filtered.
+#'
+#' @param sort                a \code{logical} if true the dates will be sorted
+#'  in increasing order. By default, the dates are sorted.
+#'
+#' @param gdal_vsi_resolution a \code{logical}  if true, gdal drivers are
+#'  included in the URL of each asset. The following schemes are supported:
+#'  HTTP/HTTPS files, S3 (AWS S3) and GS (Google Cloud Storage).
+#'
+#' @return a \code{list} with the attributes of date, bands and paths.
+#'
+#' @examples
+#' \donttest{
+#' # STACItemCollection object
+#' stac_item <- stac("http://brazildatacube.dpi.inpe.br/stac/") %>%
+#'  stac_search(collections = "CB4_64_16D_STK-1", limit = 10,
+#'         datetime = "2017-08-01/2018-03-01") %>%
+#'  get_request()
+#'
+#' stac_item %>% assets_list(assets_names = c("EVI", "NDVI"))
+#' }
 #'
 #' @export
-subfields <- function(x) {
+assets_list <- function(items, assets_names, sort = TRUE,
+                        gdal_vsi_resolution = TRUE) {
 
-  if (length(x) == 0) return(invisible(NULL))
-  names(x[[1]])
+
+  timeline <- items_reap(items, fields =  c("properties", "datetime"))
+  index    <- seq_along(timeline)
+
+  if (sort) index <- order(timeline)
+
+  timeline <- timeline[index]
+  assets   <- list(date = rep(timeline, length(unique(assets_names))))
+
+  for (b in assets_names) {
+
+    href <- items_reap(items, fields = c("assets", b, "href"))[index]
+
+    if (gdal_vsi_resolution) {
+
+      # for http or https schema
+      paste_index <- grepl("^http|[s]://.*", href)
+      if (any(paste_index))
+        href[paste_index] <- paste("/vsicurl", href[paste_index], sep = "/")
+
+      # for S3 schema
+      paste_index <- grepl("^s3://.*", href)
+      if (any(paste_index))
+        href[paste_index] <- paste("/vsis3", gsub("^s3://(.*)$", "\\1",
+                                                  href[paste_index]), sep = "/")
+      # for gs schema
+      paste_index <- grepl("^gs://.*", href)
+      if (any(paste_index))
+        href[paste_index] <- paste("/vsigs", gsub("^gs://(.*)$", "\\1",
+                                                  href[paste_index]), sep = "/")
+    }
+    assets$band <- c(rep(b, length(href)), assets$band)
+    assets$path <- c(href,  assets$path)
+  }
+  assets
 }
 
 #' @title Utility functions
 #'
-#' @description
-#' This function retrieves the values of subfields in \code{RSTACDocument}
-#'  object.
+#' @description This function returns the values of a field of the
+#'  \code{STACItemCollections} object. If the values of the specified field are
+#'  not atomic the return will be in list form, if they are, it will be returned
+#'  in vector form.
 #'
-#' @param x      a \code{list} that represents the subfields of a field of
-#'  the \code{RSTACDocument} object.
+#' @param items               a \code{STACItemCollection} object representing
+#'  the result of \code{/stac/search}, \code{/collections/{collectionId}/items}.
+#'
+#' @param ...                 a named way to provide fields names to get the
+#'  subfields values from the \code{RSTACDocument} objects.
+#'
+#' @param fields              a \code{character} with the names of the fields to
+#'  get the subfields values from the \code{RSTACDocument} objects.
+#'
+#' @return a \code{vector} if the supplied field is atomic, or a list if not.
+#'
+#' @examples
+#' \donttest{
+#' # STACItemCollection object
+#' stac_item <- stac("http://brazildatacube.dpi.inpe.br/stac/") %>%
+#'  stac_search(collections = "CB4_64_16D_STK-1", limit = 10,
+#'         datetime = "2017-08-01/2018-03-01") %>%
+#'  get_request()
+#'
+#' stac_item %>% items_reap(fields = c("properties", "datetime"))
+#' }
+#'
+#' @export
+items_reap <- function(items, ..., fields = NULL) {
+
+  # checks if the object is STACItemCollections
+  if (items_length(items) == 0) return(NULL)
+
+  dots <- substitute(list(...))[-1]
+  if (!is.character(dots)) dots <- as.character(dots)
+
+  if (length(dots) > 0 && length(fields) > 0 )
+    .error("Only one of the parameters '...' or 'fields' must be supplied.")
+
+  if (length(fields) == 0 && length(dots) == 0)
+    return(items$features)
+
+  values <- lapply(items$features, `[[`, c(dots, fields))
+
+  if (all(sapply(values, is.atomic)))
+    return(unlist(values))
+  values
+}
+
+#' @title Utility functions
+#'
+#' @description This function returns the subfields of the \code{feature}
+#' field of a \code{STACItemCollection} object.
+#'
+#' @param items  a \code{STACItemCollection} object representing
+#'  the result of \code{/stac/search}, \code{/collections/{collectionId}/items}.
 #'
 #' @param ...    a named way to provide fields names to get the subfields values
 #'  from the \code{RSTACDocument} objects.
@@ -443,40 +549,105 @@ subfields <- function(x) {
 #' @param fields a \code{character} with the names of the fields to get the
 #'  subfields values from the \code{RSTACDocument} objects.
 #'
-#' @param merge  a \code{logical} corresponds to the join of the subfields. If
-#'  true, the subfields are joined in a named list. If not, the subfields appear
-#'  in their respective index of the original list.
+#' @return a \code{character} with the subfields of the \code{feature} field.
 #'
-#' @return a \code{list} with the values of the selected subfields.
+#' @examples
+#' \donttest{
+#' # STACItemCollection object
+#' stac_item <- stac("http://brazildatacube.dpi.inpe.br/stac/") %>%
+#'  stac_search(collections = "CB4_64_16D_STK-1", limit = 10,
+#'         datetime = "2017-08-01/2018-03-01") %>%
+#'  get_request()
+#'
+#' stac_item %>% items_fields(fields = c("properties"))
+#' }
 #'
 #' @export
-get_subfields <- function(x, ..., fields = NULL, merge = TRUE) {
+items_fields <- function(items, ..., fields = NULL) {
 
-  if (length(x) == 0) return(invisible(NULL))
+  # checks if the object is STACItemCollections
+  if (items_length(items) == 0) return(NULL)
+
   dots <- substitute(list(...))[-1]
   if (!is.character(dots)) dots <- as.character(dots)
-  fields <- c(fields, dots)
-  if (length(fields) == 0) fields <- names(x[[1]])
-  x <- lapply(x, function(y) {
-    y <- y[fields]
-    names(y) <- fields
-    y
+
+  if (length(fields) > 0 && length(dots) > 0)
+    .error("Only one of the parameters '...' or 'fields' must be supplied.")
+
+  if (length(fields) == 0 && length(dots) == 0)
+    return(names(items$features[[1]]))
+  names(items$features[[1]][[c(dots, fields)]])
+}
+
+
+#' @title Utility functions
+#'
+#' @description This function groups the items contained within the
+#'  \code{STACItemCollection} object according to some specified fields. Each
+#'  index in the returned list contains items belonging to the same group.
+#'
+#' @param items               a \code{STACItemCollection} object representing
+#'  the result of \code{/stac/search}, \code{/collections/{collectionId}/items}.
+#'
+#' @param ...    a named way to provide fields names to get the subfields values
+#'  from the \code{RSTACDocument} objects.
+#'
+#' @param index a \code{character} with the indexes to be grouped. It can be
+#'  used with the function \link{items_reap}.
+#'
+#' @param fields a \code{character} with the names of the fields to get the
+#'  subfields values from the \code{RSTACDocument} objects.
+#'
+#' @return a \code{list} in which each index corresponds to a group with its
+#'  corresponding \code{STACItemCollection} objects.
+#'
+#' @examples
+#' \donttest{
+#' # STACItemCollection object
+#' stac_item <- stac("http://brazildatacube.dpi.inpe.br/stac/") %>%
+#'  stac_search(collections = "CB4_64_16D_STK-1", limit = 10,
+#'         datetime = "2017-08-01/2018-03-01") %>%
+#'  get_request()
+#'
+#'  stac_item %>% items_group(., fields = c("properties", "bdc:tile"))
+#' }
+#'
+#' @export
+items_group <- function(items, ..., index = NULL, fields = NULL) {
+
+  # checks if the object is STACItemCollections
+  if (items_length(items) == 0) return(list(items))
+
+  dots <- substitute(list(...))[-1]
+  if (!is.character(dots)) dots <- as.character(dots)
+
+  if (length(index) == 0 && length(fields) == 0 &&  length(dots) == 0)
+    .error(paste("Either parameters 'index', 'fields' or '...' parameters must",
+                 "be supplied."))
+
+  if (length(index) > 0 && (length(fields) > 0 || length(dots) > 0))
+    .error(paste("Only one of the parameters '...','index' or 'fields' should",
+                 "be supplied."))
+
+  if (is.null(index))
+    index <- items_reap(items, ..., fields = fields)
+
+  if (items_length(items) != length(index))
+    .error(paste("The length of the field provided for grouping must contain",
+                 "the same size as the length of the items."))
+
+  if (!is.atomic(index))
+    .error("Error ...")
+
+  features <- unname(tapply(X = items$features,
+                            INDEX = index,
+                            FUN = c, simplify = FALSE))
+
+  lapply(features, function(x){
+    items$features <- x
+
+    items
   })
-  x <- lapply(x, `names<-`, fields)
-  if (merge)
-    x <- do.call(mapply,
-                 c(list(FUN = function(...) {
-                   # equivalent to `c` but convert NULL --> NA
-                   dots <- list(...)
-                   if (length(dots) == 0)
-                     return(NULL)
-                   sapply(dots, function(x) {
-                     if (is.null(x)) return(NA); x
-                   }, simplify = is.atomic(dots[[1]]) && length(dots[[1]]) == 1)
-                 },
-                 SIMPLIFY = FALSE),
-                 unname(x)))
-  x
 }
 
 #' @title Utility functions
