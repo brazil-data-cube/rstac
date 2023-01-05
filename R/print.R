ec2c4388b862494cb11c17f9be1032da482562e0
Felipe <lipecaso@gmail.com>
2023-01-05 18:12:50 +0000

    remove simplify parameter in calls of items_assets function

--- R/print.R
+++ R/print.R
@@ -179,7 +179,7 @@ print.STACItemCollection <- function(x, n = 10, ..., tail = FALSE) {
                 length(x$features) - n), fill = TRUE)
 
   cat("-", crayon::bold("assets:"),
-      paste0(items_assets(x, simplify = TRUE), collapse = ", "),
+      paste0(items_assets(x), collapse = ", "),
       fill = TRUE)
   cat("-", crayon::bold("other field(s):"),
       paste0(names(x), collapse = ", "), fill = TRUE)
@@ -197,7 +197,7 @@ print.STACItem <- function(x, ...) {
   cat("-", crayon::bold("bbox:"), format_bbox(x$bbox), fill = TRUE)
   cat("-", crayon::bold("datetime:"), x$properties$datetime, fill = TRUE)
   cat("-", crayon::bold("assets:"),
-      paste0(items_assets(x, simplify = TRUE), collapse = ", "),
+      paste0(items_assets(x), collapse = ", "),
       fill = TRUE)
   cat("-", crayon::bold("other field(s):"), paste0(names(x), collapse = ", "),
       fill = TRUE)
