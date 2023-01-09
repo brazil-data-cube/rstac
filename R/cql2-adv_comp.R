6bc6771a9cb3a81e2b23e22451089d8ae31cf26c
Rolf Simoes <rolfsimoes@gmail.com>
2023-01-09 09:59:29 -0300

    Improve code style

--- R/cql2-adv_comp.R
+++ R/cql2-adv_comp.R
@@ -9,34 +9,34 @@
 
 # like_op
 like_op <- function(a, b) {
-    a <- cql2_eval(a)
-    b <- cql2_eval(b)
-    stopifnot(is_str_expr(a))
-    stopifnot(is_patt_expr(b))
-    structure(list(op = "like", args = list(a, b)),
-              class = c("cql2_like_op", "list"))
+  a <- cql2_eval(a)
+  b <- cql2_eval(b)
+  stopifnot(is_str_expr(a))
+  stopifnot(is_patt_expr(b))
+  structure(list(op = "like", args = list(a, b)),
+            class = c("cql2_like_op", "list"))
 }
 
 # between_op
 between_op <- function(a, b, c) {
-    a <- cql2_eval(a)
-    b <- cql2_eval(b)
-    c <- cql2_eval(c)
-    stopifnot(is_num_expr(a))
-    stopifnot(is_num_expr(b))
-    stopifnot(is_num_expr(c))
-    structure(list(op = "between", args = list(a, b, c)),
-              class = c("cql2_between_op", "list"))
+  a <- cql2_eval(a)
+  b <- cql2_eval(b)
+  c <- cql2_eval(c)
+  stopifnot(is_num_expr(a))
+  stopifnot(is_num_expr(b))
+  stopifnot(is_num_expr(c))
+  structure(list(op = "between", args = list(a, b, c)),
+            class = c("cql2_between_op", "list"))
 }
 
 # in_op
 in_op <- function(a, b) {
-    a <- cql2_eval(a)
-    b <- cql2_eval(b)
-    stopifnot(is_scalar(a))
-    stopifnot(is_scalar_lst(b))
-    structure(list(op = "in", args = list(a, b)),
-              class = c("cql2_in_op", "list"))
+  a <- cql2_eval(a)
+  b <- cql2_eval(b)
+  stopifnot(is_scalar(a))
+  stopifnot(is_scalar_lst(b))
+  structure(list(op = "in", args = list(a, b)),
+            class = c("cql2_in_op", "list"))
 }
 
 casei <- function(a) {
@@ -55,22 +55,22 @@ accenti <- function(a) {
 
 # spatial_op
 spatial_op <- function(op) {
-    function(a, b) {
-        a <- cql2_eval(a)
-        b <- cql2_eval(b)
-        stopifnot(is_spatial_expr(a))
-        stopifnot(is_spatial_expr(b))
-        if (is_spatial(a))
-            a <- get_spatial(a)
-        if (is_spatial(b))
-            b <- get_spatial(b)
-        structure(list(op = op, args = list(a, b)),
-                  class = c("cql2_spatial_op", "list"))
-    }
+  function(a, b) {
+    a <- cql2_eval(a)
+    b <- cql2_eval(b)
+    stopifnot(is_spatial_expr(a))
+    stopifnot(is_spatial_expr(b))
+    if (is_spatial(a))
+      a <- get_spatial(a)
+    if (is_spatial(b))
+      b <- get_spatial(b)
+    structure(list(op = op, args = list(a, b)),
+              class = c("cql2_spatial_op", "list"))
+  }
 }
 
 get_spatial <- function(x) {
-    UseMethod("get_spatial", x)
+  UseMethod("get_spatial", x)
 }
 
 spatial_types <- c("Point", "MultiPoint", "LineString",
@@ -79,16 +79,20 @@ spatial_types <- c("Point", "MultiPoint", "LineString",
 
 #' @export
 get_spatial.character <- function(x) {
-  class(x) <- c("cql2_spatial", "character")
+  x <- jsonlite::fromJSON(x,
+                          simplifyVector = TRUE,
+                          simplifyDataFrame = FALSE,
+                          simplifyMatrix = FALSE)
+  class(x) <- c("cql2_spatial", "list")
   x
 }
 
 #' @export
 get_spatial.list <- function(x) {
-    stopifnot(c("type", "coordinates") %in% names(x))
-    stopifnot(x[["type"]] %in% spatial_types)
-    class(x) <- c("cql2_spatial", "list")
-    x
+  stopifnot(c("type", "coordinates") %in% names(x))
+  stopifnot(x[["type"]] %in% spatial_types)
+  class(x) <- c("cql2_spatial", "list")
+  x
 }
 
 #' @export
@@ -99,34 +103,34 @@ get_spatial.sfc <- function(x) get_spatial.sfg(x[[1]])
 
 #' @export
 get_spatial.sfg <- function(x) {
-    names(spatial_types) <- toupper(spatial_types)
+  names(spatial_types) <- toupper(spatial_types)
 
-    geom_type <- spatial_types[[as.character(sf::st_geometry_type(x))]]
-    structure(
-        list(type = geom_type, coordinates = unclass(x)),
-        class = c("cql2_spatial", "list")
-    )
+  geom_type <- spatial_types[[as.character(sf::st_geometry_type(x))]]
+  structure(
+    list(type = geom_type, coordinates = unclass(x)),
+    class = c("cql2_spatial", "list")
+  )
 }
 
 #' @export
 get_spatial.GEOMETRYCOLLECTION <- function(x) {
-    structure(
-        list(type = "GeometryCollection", geometries = lapply(x, get_spatial)),
-        class = c("cql2_spatial", "list")
-    )
+  structure(
+    list(type = "GeometryCollection", geometries = lapply(x, get_spatial)),
+    class = c("cql2_spatial", "list")
+  )
 }
 
 # temporal_op
 temporal_op <- function(op) {
-    function(a, b) {
-        a <- cql2_eval(a)
-        b <- cql2_eval(b)
-        stopifnot(is_temporal_expr(a))
-        stopifnot(is_temporal_expr(b))
+  function(a, b) {
+    a <- cql2_eval(a)
+    b <- cql2_eval(b)
+    stopifnot(is_temporal_expr(a))
+    stopifnot(is_temporal_expr(b))
 
-        structure(list(op = op, args = list(a, b)),
-                  class = c("cql2_temporal_op", "list"))
-    }
+    structure(list(op = op, args = list(a, b)),
+              class = c("cql2_temporal_op", "list"))
+  }
 }
 
 # array_op
@@ -158,31 +162,31 @@ to_json.cql2_in_op <- function(x) json_obj(x)
 # like_op
 #' @export
 text_not_op.cql2_like_op <- function(x)
-    paste(to_text(x$args[[1]]$args[[1]]), "NOT LIKE",
-          to_text(x$args[[1]]$args[[2]]))
+  paste(to_text(x$args[[1]]$args[[1]]), "NOT LIKE",
+        to_text(x$args[[1]]$args[[2]]))
 
 #' @export
 to_text.cql2_like_op <- function(x)
-    paste(to_text(x$args[[1]]), toupper(x$op), to_text(x$args[[2]]))
+  paste(to_text(x$args[[1]]), toupper(x$op), to_text(x$args[[2]]))
 
 # between_op
 #' @export
 text_not_op.cql2_between_op <- function(x)
-    paste(to_text(x$args[[1]]$args[[1]]), "NOT BETWEEN",
-          to_text(x$args[[1]]$args[[2]]), "AND",
-          to_text(x$args[[1]]$args[[3]]))
+  paste(to_text(x$args[[1]]$args[[1]]), "NOT BETWEEN",
+        to_text(x$args[[1]]$args[[2]]), "AND",
+        to_text(x$args[[1]]$args[[3]]))
 
 #' @export
 to_text.cql2_between_op <- function(x)
-    paste(to_text(x$args[[1]]), "BETWEEN",
-          to_text(x$args[[2]]), "AND", to_text(x$args[[3]]))
+  paste(to_text(x$args[[1]]), "BETWEEN",
+        to_text(x$args[[2]]), "AND", to_text(x$args[[3]]))
 
 # in_op
 #' @export
 text_not_op.cql2_in_op <- function(x)
-    paste(to_text(x$args[[1]]$args[[1]]), "NOT IN",
-          to_text(x$args[[1]]$args[[2]]))
+  paste(to_text(x$args[[1]]$args[[1]]), "NOT IN",
+        to_text(x$args[[1]]$args[[2]]))
 
 #' @export
 to_text.cql2_in_op <- function(x)
-    paste(to_text(x$args[[1]]), "IN", to_text(x$args[[2]]))
+  paste(to_text(x$args[[1]]), "IN", to_text(x$args[[2]]))
