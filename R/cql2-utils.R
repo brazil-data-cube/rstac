e0f2033763a46d3566e1f73c2df558e35c2795c4
Rolf Simoes <rolfsimoes@gmail.com>
2023-01-09 10:00:12 -0300

    Improve new functions and documentation

--- R/cql2-utils.R
+++ R/cql2-utils.R
@@ -1,11 +1,25 @@
-
+#' CQL2 helper function
+#'
+#' Function to convert bounding box (bbox) to a GeoJSON object
+#' to be used as argument of CQL2 spatial operators.
+#'
+#' @param bbox  A `numeric` containing a bbox with c(xmin, ymin, xmax, ymax).
+#'
+#' @return GeoJSON object
+#'
+#' @examples
+#' \dontrun{
+#'
+#'
+#' }
+#'
 #' @export
-cql2_bbox <- function(bbox) {
+cql2_bbox_as_geojson <- function(bbox) {
   # bbox = c(xmin, ymin, xmax, ymax)
   list(
     type = "Polygon",
     coordinates = list(
-      list(
+      rbind(
         c(bbox[[1]], bbox[[2]]),
         c(bbox[[3]], bbox[[2]]),
         c(bbox[[3]], bbox[[4]]),
@@ -16,18 +30,43 @@ cql2_bbox <- function(bbox) {
   )
 }
 
+#' CQL2 temporal types
+#'
+#' These are helper functions to create temporal literal values to be
+#' passed into CQL2 expressions.
+#'
+#' @param x,start,end `character` string containing valid `date` or `timestamp`.
+#'
+#' @return CQL2 temporal value
+#'
+#' @name cql2_temporal
+#'
+#' @examples
+#' \dontrun{
+#'
+#'
+#' }
+#'
+NULL
+
+#' @rdname cql2_temporal
+#'
 #' @export
 cql2_date <- function(x) {
   stopifnot(is_date(x))
   unquote(quote(date({{x}})), environment())
 }
 
+#' @rdname cql2_temporal
+#'
 #' @export
 cql2_timestamp <- function(x) {
   stopifnot(is_time(x))
   unquote(quote(timestamp({{x}})), environment())
 }
 
+#' @rdname cql2_temporal
+#'
 #' @export
 cql2_interval <- function(start = "..", end = "..") {
   if (start != "..")
