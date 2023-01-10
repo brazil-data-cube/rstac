441537e618f81acae2ffa3b113069165ff94915f
Rolf Simoes <rolfsimoes@gmail.com>
2023-01-10 12:24:41 -0300

    Update package documentation index

--- R/rstac.R
+++ R/rstac.R
@@ -7,29 +7,39 @@
 #' @section STAC API endpoints functions:
 #' \itemize{
 #'   \item [stac()]: implements STAC `/stac` endpoint for version
-#'    0.8.1 or below, and `/` for versions 0.9.0 or higher.
+#'     0.8.1 or below, and `/` for versions 0.9.0 or higher.
+#'   \item [conformance()]: implements `/conformance` endpoint.
 #'   \item [collections()]: implements `/collections`
-#'     and \code{/collections/\{collectionId\}} WFS3 endpoints.
+#'     and \code{/collections/\{collectionId\}} endpoints.
 #'   \item [items()]: implements
 #'     \code{/collections/\{collectionId\}/items} and
-#'     \code{/collections/\{collectionId\}/items/\{featureId\}} WFS3 endpoints.
+#'     \code{/collections/\{collectionId\}/items/\{featureId\}} endpoints.
+#'   \item [queryables()]: implements `/queryables` and
+#'     \code{/collections/\{collectionId\}/queryables} endpoints.
 #'   \item [stac_search()]: implements STAC `/stac/search`
 #'   endpoint for version 0.8.1 or below, and `/search` endpoint for
 #'   versions 0.9.0 or higher.
+#'   \item [ext_filter()]: implements `/filter` CQL2 endpoint.
 #' }
 #'
 #' @section Data access and organization functions:
 #' \itemize{
-#'   \item [get_request()]: makes HTTP GET requests to STAC web
-#'   service.
-#'   \item [post_request()]: makes HTTP POST requests to STAC web
-#'   service.
+#'   \item [get_request()]: makes HTTP GET requests to STAC web service.
+#'   \item [post_request()]: makes HTTP POST requests to STAC web service.
 #'   \item [items_matched()]: returns how many items matched the
 #'   search criteria.
-#'   \item [items_length()]: informs how many items are
-#'   stored locally.
 #'   \item [items_fetch()]: fetches all matched items from service.
-#'   \item [assets_download()]: download all assets in batch.
+#'   \item [items_filter()]: selects items according to some criteria.
+#'   \item [items_as_sf()]: converts items to a `sf` object.
+#'   \item [items_field()]: help explore fields inside items.
+#'   \item [items_reap()]: extracts contents from items.
+#'   \item [items_length()]: informs how many items are fetched locally.
+#'   \item [items_sign()]: appends tokens to assets' URL and turn
+#'   them accessible.
+#'   \item [assets_select()]: select assets in items.
+#'   \item [assets_rename()]: rename assets in items.
+#'   \item [assets_url()]: extract all URL to assets in items.
+#'   \item [assets_download()]: download assets in batch.
 #' }
 #'
 #' @section Data types:
