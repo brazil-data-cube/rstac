c780d24338b012d6ea4a2328fda63e22ab2255d4
Rolf Simoes <rolfsimoes@gmail.com>
2023-01-12 19:33:25 -0300

    Removed unused functions add apply_deeply() function

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,4 @@
-#' @title Utility functions
-#'
-#' @description Function similar to modifyList of utils, however it is checked
-#'  if the left parameter is null.
-#'
-#' @param x   a `list` to be compared in left side.
-#'
-#' @param y   a `list` to be compared in right side.
-#'
-#' @return a `list` with modified values.
-#'
-#' @noRd
+
 modify_list <- function(x, y) {
   if (is.null(x))
     x <- list()
@@ -42,143 +31,24 @@ map_chr <- function(x, fn, ..., use_names = FALSE) {
   vapply(x, fn, FUN.VALUE = character(1), ..., USE.NAMES = use_names)
 }
 
-# nocov start
-links_filter <- function(x, ..., filter_fn = NULL) {
-
-  stopifnot("links" %in% names(x))
-
-  # check items parameter
-  check_subclass(x, c("STACItem", "STACItemCollection", "STACCatalog",
-                      "STACCollection", "STACCollectionList"))
-
-  dots <- substitute(list(...))[-1]
-
-  if (length(dots) > 0) {
-
-    if (!is.null(names(dots)))
-      .error("Invalid filter arguments.")
-
-    for (i in seq_along(dots)) {
-
-      sel <- map_lgl(x$links, function(l) {
-        eval(dots[[i]], envir = l)
-      })
-
-      x$links <- x$links[sel]
+apply_deeply <- function(x, i = NULL, fn = identity, ...) {
+  val <- if (length(i) == 0) {
+    fn(x, ...)
+  } else {
+    star <- which(i == "*")
+    if (length(star) == 0) {
+      x <- tryCatch(x[[i]], error = function(e) NULL)
+      if (is.null(x)) NULL else fn(x, ...)
+    } else if (star[[1]] == 1) {
+      lapply(x, apply_deeply, i = i[-1], fn = fn)
+    } else {
+      x <- tryCatch(x[[i[seq_len(star[[1]] - 1)]]], error = function(e) NULL)
+      if (is.null(x)) NULL
+      else lapply(x, apply_deeply, i = i[-seq(star[[1]])], fn = fn)
     }
   }
-
-  if (!is.null(filter_fn)) {
-
-    sel <- map_lgl(x$links, function(l) {
-      filter_fn(l$links)
-    })
-
-    x$links <- x$links[sel]
-  }
-
-  x$links
+  if (is.null(names(val)) &&
+      all(vapply(val, function(x) is.atomic(x) && length(x) == 1, logical(1))))
+    return(unlist(val, recursive = FALSE))
+  return(val)
 }
-
-.field_filter <- function(x, field, ..., filter_fn = NULL) {
-
-  stopifnot(field %in% names(x))
-
-  x <- x[[field]]
-  stopifnot(!is.atomic(x))
-
-  dots <- .check_unnamed(.capture_dots(...))
-
-  if (length(dots) > 0) {
-
-    if (!is.null(names(dots)))
-      .error("Invalid filter arguments.")
-
-    for (i in seq_along(dots)) {
-
-      sel <- map_lgl(x, function(xi) {
-        eval(dots[[i]], envir = xi)
-      })
-
-      x <- x[sel]
-    }
-  }
-
-  if (!is.null(filter_fn)) {
-
-    sel <- map_lgl(x, function(xi) {
-      filter_fn(xi)
-    })
-
-    x <- x[sel]
-  }
-
-  x
-}
-
-.field_apply <- function(x, field, apply_fn, ...) {
-
-  stopifnot(field %in% names(x))
-
-  x <- x[[field]]
-  stopifnot(!is.atomic(x))
-
-  lapply(x, apply_fn, ...)
-}
-
-.field_mutate <- function(x, field, ..., apply_fn = NULL) {
-
-  stopifnot(field %in% names(x))
-
-  x <- x[[field]]
-  stopifnot(!is.atomic(x))
-
-  dots <- .check_named(.capture_dots(...))
-  stopifnot(length(dots) > 0 && is.null(apply_fn))
-
-  if (length(dots) > 0) {
-
-    if (!is.null(names(dots)))
-      .error("Invalid filter arguments.")
-
-    value_d <- lapply(dots, function(di) {
-
-      lapply(x, function(xi) eval(di, envir = xi))
-    })
-  }
-
-  if (!is.null(apply_fn)) {
-    value_fn <- map_lgl(x, function(xi) {
-      apply_fn(xi)
-    })
-
-    x <- x[value_fn]
-  }
-
-  x
-}
-
-.capture_dots <- function(...) {
-
-  lapply(substitute(list(...), env = environment()), unlist, recursive = F)[-1]
-}
-
-.is_named <- function(x) {
-
-  !is.null(names(x)) && all(nzchar(names(x)))
-}
-
-.check_named <- function(x) {
-
-  stopifnot(.is_named(x))
-
-  x
-}
-
-.check_unnamed <- function(x) {
-
-  stopifnot(!.is_named(x))
-
-  x
-}
-# nocov end
